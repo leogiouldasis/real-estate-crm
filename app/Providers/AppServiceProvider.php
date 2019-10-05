bbb4ded3c83ce5afa9b18ccfb34b02288762bb07
Leo Giouldasis <leo.giouldasis@gmail.com>
2019-10-05 21:00:06 +0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (in_array(env('APP_ENV'), ['staging', 'production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
